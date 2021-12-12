c3aaebd7428e0fc79b55e858c39033d041edbfc0
Ade Rohayat <muhamadaderohayat122@gmail.com>
2021-12-12 16:08:22 +0700

    DYNAMIC ACTIVE DASHBOARD

--- components/Layout/member.tsx
+++ components/Layout/member.tsx
@@ -11,11 +11,17 @@ interface LayoutProps {
 export default function MemberLayout(props: LayoutProps) {
   const { children, pageTitle, classSection } = props;
   const section = `${classSection} overflow-auto`;
-  // const menuActive = {
-  //   overview: (classSection === 'overview') ? 'active' : '',
-  //   transactions: (classSection === 'transactions') ? 'active' : '',
-  // };
-  // active
+
+  function menuCheck(thisClass: string) {
+    const result = (classSection === thisClass) ? 'active' : '';
+    return `${result} item mb-30`;
+  }
+
+  const menuActive = {
+    overview: menuCheck('overview'),
+    transactions: menuCheck((classSection === 'transactions-detail') ? 'transactions-detail' : 'transactions'),
+    editProfile: menuCheck('edit-profile'),
+  };
   return (
     <>
       <Head>
@@ -35,8 +41,8 @@ export default function MemberLayout(props: LayoutProps) {
             </div>
             {/* Menu */}
             <div className="menus">
-              {/* Overview */}
-              <div className="item mb-30">
+              {/* overview */}
+              <div className={menuActive.overview}>
                 <svg className="icon me-3" width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M21.9033 14.7502H14.9033V21.7502H21.9033V14.7502Z" stroke="#7E8CAC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                   <path d="M10.9033 14.7502H3.90332V21.7502H10.9033V14.7502Z" stroke="#7E8CAC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -49,8 +55,8 @@ export default function MemberLayout(props: LayoutProps) {
                   </Link>
                 </p>
               </div>
-              {/* Transactions */}
-              <div className="item mb-30">
+              {/* transactions */}
+              <div className={menuActive.transactions}>
                 <svg className="icon me-3" width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M6.90332 2.41406L3.90332 6.41406V20.4141C3.90332 20.9445 4.11403 21.4532 4.48911 21.8283C4.86418 22.2033 5.37289 22.4141 5.90332 22.4141H19.9033C20.4338 22.4141 20.9425 22.2033 21.3175 21.8283C21.6926 21.4532 21.9033 20.9445 21.9033 20.4141V6.41406L18.9033 2.41406H6.90332Z" stroke="#7E8CAC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                   <path d="M16.9033 10.4141C16.9033 11.4749 16.4819 12.4923 15.7317 13.2425C14.9816 13.9926 13.9642 14.4141 12.9033 14.4141C11.8425 14.4141 10.825 13.9926 10.0749 13.2425C9.32475 12.4923 8.90332 11.4749 8.90332 10.4141" stroke="#7E8CAC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -105,8 +111,8 @@ export default function MemberLayout(props: LayoutProps) {
                   <a href="/#" className="text-lg text-decoration-none">Rewards</a>
                 </p>
               </div>
-              {/* Settings */}
-              <div className="item mb-30">
+              {/* editProfile */}
+              <div className={menuActive.editProfile}>
                 <svg className="icon me-3" width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <g clip-path="url(#clip0)">
                     <path d="M12.9033 15.4141C14.5602 15.4141 15.9033 14.0709 15.9033 12.4141C15.9033 10.7572 14.5602 9.41406 12.9033 9.41406C11.2465 9.41406 9.90332 10.7572 9.90332 12.4141C9.90332 14.0709 11.2465 15.4141 12.9033 15.4141Z" stroke="#7E8CAC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
